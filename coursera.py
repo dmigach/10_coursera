2e728f5b970c7d912b61678ebccde32ea7f7c61b
saceth1 <sacethi0@gmail.com>
2017-06-09 11:56:30 +0300

    variable names edited to get rid of docstrings; global variables deleted; Course class and indent in ifmain are no longer exist

--- coursera.py
+++ coursera.py
@@ -2,149 +2,96 @@ import argparse
 import json
 import os
 import re
+import sys
 import textwrap
 
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
 import requests
 
-coursera_xml_feed_url = 'https://www.coursera.org/sitemap~www~courses.xml'
-
-search_classes = {
-    'course_name': 'title display-3-text',
-    'course_language': 'language-info',
-    'weeks_tag': 'rc-WeekView',
-    'week_tag': 'week'
-}
-
 
 def get_soup_from_url(url):
-    """
-    :param url: url for html request
-    :return: Beautiful Soup object
-    """
     request = requests.get(url)
     request.encoding = 'utf-8'
     if request.status_code == 200:
         return BeautifulSoup(request.text, 'lxml')
 
 
-def get_courses_list(url, amount):
-    """
-    :param url: url for xml feed with courses list
-    :param amount: amount of desired courses
-    :return: list with courses urls
-    """
-    courses_list_soup = get_soup_from_url(url)
+def get_courses_list(xml_feed_url, amount_of_courses):
+    courses_list_soup = get_soup_from_url(xml_feed_url)
     if courses_list_soup:
-        courses_urls = courses_list_soup.find_all('loc', limit=amount)
-        return [tag.text for tag in courses_urls]
+        courses_urls = courses_list_soup.find_all('loc',
+                                                  limit=amount_of_courses)
+        list_of_courses_urls = [tag.text for tag in courses_urls]
+        return list_of_courses_urls
 
 
 def parse_courses(urls_list):
-    """
-    :param urls_list: list of courses urls
-    :return: list of dictionaries with courses info
-    """
-    courses_list = []
+    list_of_dictionaries_with_courses_info = []
     for url in urls_list:
         course_soup = get_soup_from_url(url)
-        course = Course(course_soup)
-        courses_list.append(course.get_course_info())
-    return courses_list
+        course_info = get_course_info(course_soup)
+        list_of_dictionaries_with_courses_info.append(course_info)
+    return list_of_dictionaries_with_courses_info
 
 
-class Course:
-    """
-    I had to create this class because my course parse function had MacCabe
-    function complexity score of 13 and i didn't want to pass heavy soup
-    object to every single parse function
-    """
-    def __init__(self, soup):
-        """
-        :param soup: Beautiful Soup of course main page
-        """
-        self.soup = soup
-
-    def get_name(self):
-        """
-        :return: string with course name
-        """
-        try:
-            return self.soup.find(
-                class_=search_classes['course_name']).text
-        except AttributeError:
-            return '-'
-
-    def get_language(self):
-        """
-        :return: string with course language
-        """
-        try:
-            return self.soup.find(
-                class_=search_classes['course_language']).text
-        except AttributeError:
-            return '-'
-
-    def get_duration(self):
-        """
-        :return: integer with course duration
-        """
-        try:
-            weeks_list = self.soup.find(
-                class_=search_classes['weeks_tag']).find_all(
-                class_=search_classes['week_tag'])
-            return len(weeks_list)
-        except AttributeError:
-            return '-'
-
-    def get_average_score(self):
-        """
-        :return: string with average course score
-        """
-        try:
-            script_tag = self.soup.find('script',
-                                        text=re.compile('window.App')).text
-            return re.findall(r'"averageFiveStarRating":([\d.]+)',
-                              script_tag)[0]
-        except (AttributeError, IndexError):
-            return '-'
-
-    def get_start_date(self):
-        """
-        :return: string with course start date
-        """
-        try:
-            course_script_vars = json.loads(self.soup.find('script',
-                                            type='application/ld+json').text.
-                                            strip())
-            return course_script_vars['hasCourseInstance'][0]['startDate']
-        except (AttributeError, IndexError, KeyError):
-            return '-'
-
-    def get_course_info(self):
-        """
-        :return: dictionary with course info. Keys: name, language, weeks,
-         average_score, start_date
-        """
-        course_dictionary = {'name': self.get_name(),
-                             'language': self.get_language(),
-                             'weeks': self.get_duration(),
-                             'average_score': self.get_average_score(),
-                             'start_date': self.get_start_date()}
-        return course_dictionary
+def get_course_name(soup):
+    course_name_class = 'title display-3-text'
+    try:
+        return soup.find(class_=course_name_class).text
+    except AttributeError:
+        return '-'
 
 
-def output_courses_info_to_xlsx(workbook, file_path, courses_info,
+def get_course_language(soup):
+    course_language_class = 'language-info'
+    try:
+        return soup.find(class_=course_language_class).text
+    except AttributeError:
+        return '-'
+
+
+def get_course_duration(soup):
+    course_weeks_tag = 'rc-WeekView'
+    week_tag = 'week'
+    try:
+        weeks_list = soup.find(class_=course_weeks_tag).find_all(
+            class_=week_tag)
+        return len(weeks_list)
+    except AttributeError:
+        return '-'
+
+
+def get_course_average_score(soup):
+    try:
+        script_tag = soup.find('script', text=re.compile('window.App')).text
+        return re.findall(r'"averageFiveStarRating":([\d.]+)',
+                          script_tag)[0]
+    except (AttributeError, IndexError):
+        return '-'
+
+
+def get_course_start_date(soup):
+    try:
+        course_script_vars = json.loads(soup.find('script',
+                                        type='application/ld+json').text.
+                                        strip())
+        return course_script_vars['hasCourseInstance'][0]['startDate']
+    except (AttributeError, IndexError, KeyError):
+        return '-'
+
+
+def get_course_info(soup):
+    course_dictionary = {'name': get_course_name(soup),
+                         'language': get_course_language(soup),
+                         'weeks': get_course_duration(soup),
+                         'average_score': get_course_average_score(soup),
+                         'start_date': get_course_start_date(soup)}
+    return course_dictionary
+
+
+def output_courses_info_to_xlsx(workbook, save_file_path, courses_info,
                                 name_column_width, language_column_width):
-    """
-    :param workbook: openpyxl Workbook object
-    :param file_path: file path for file saving
-    :param courses_info: list of dictionaries with courses info
-    :param name_column_width:
-    :param language_column_width:
-    :return: None
-    """
     sheet = workbook.active
     for row, course in enumerate(courses_info):
         sheet.cell(row=row + 2, column=1).value = textwrap.fill(
@@ -154,21 +101,12 @@ def output_courses_info_to_xlsx(workbook, file_path, courses_info,
         sheet.cell(row=row + 2, column=3).value = course['start_date']
         sheet.cell(row=row + 2, column=4).value = course['weeks']
         sheet.cell(row=row + 2, column=5).value = course['average_score']
-    file_path = os.path.join(file_path)
-    workbook.save(file_path)
+    save_file_path = os.path.join(save_file_path)
+    workbook.save(save_file_path)
 
 
 def setup_excel_workbook(name_width, language_width, date_width=16,
                          duration_width=9, average_score_width=16):
-    """
-    Prepares excel workbook for filling
-    :param name_width:
-    :param language_width:
-    :param date_width:
-    :param duration_width:
-    :param average_score_width:
-    :return: openpyxl's Workbook object
-    """
     workbook = Workbook()
     sheet = workbook.active
     sheet.column_dimensions['A'].width = name_width
@@ -185,10 +123,6 @@ def setup_excel_workbook(name_width, language_width, date_width=16,
 
 
 def parse_arguments():
-    """
-    :return: tuple of length 4: courses amount, name column width, language
-      column width, output file name
-    """
     parser = argparse.ArgumentParser(description='Write Coursera courses info'
                                                  ' to xlsx')
     parser.add_argument('amount', nargs='?', default=20,
@@ -204,25 +138,29 @@ def parse_arguments():
         arguments.output
 
 
+def append_xlsx_file_extension_if_needed(file_name):
+    return file_name + '.xlsx' if '.xlsx' not in file_name else file_name
+
+
 if __name__ == '__main__':
+    coursera_xml_feed_url = 'https://www.coursera.org/sitemap~www~courses.xml'
     courses_amount, name_col_width, language_col_width, output_file_name =\
         parse_arguments()
 
-    if '.xlsx' not in output_file_name:
-        output_file_name += '.xlsx'
+    output_file_name = append_xlsx_file_extension_if_needed(output_file_name)
 
-    courses_urls_list = get_courses_list(coursera_xml_feed_url, courses_amount)
     print('---Getting course list...')
+    courses_urls_list = get_courses_list(coursera_xml_feed_url, courses_amount)
 
-    if courses_urls_list:
-        courses = parse_courses(courses_urls_list)
-        print('---Parsing courses info. This can take a while (few seconds for'
-              ' every course).')
+    sys.exit('!!! Something went wrong: check your internet connection;'
+             ' also coursera xml feed url may changed'
+             ) if not courses_urls_list else None
 
-        wb = setup_excel_workbook(name_col_width, language_col_width)
-        output_courses_info_to_xlsx(wb, output_file_name, courses,
-                                    name_col_width, language_col_width)
-        print('---Success: {} is ready!'.format(output_file_name))
-    else:
-        print('!!! Something went wrong: check your internet connection;'
-              ' also coursera xml feed may changed')
+    courses = parse_courses(courses_urls_list)
+    print('---Parsing courses info. This can take a while (few seconds for'
+          ' every course).')
+
+    wb = setup_excel_workbook(name_col_width, language_col_width)
+    output_courses_info_to_xlsx(wb, output_file_name, courses,
+                                name_col_width, language_col_width)
+    print('---Success: {} is ready!'.format(output_file_name))
